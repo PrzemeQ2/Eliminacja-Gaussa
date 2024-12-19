990ecdaff433660b2b4ec1016ed4dc8838e02fb7
Maciek Palkowski <maciej.palkowski.stud@pw.edu.pl>
2024-12-19 18:42:42 +0100

    implementacja podstawienia wstecznego

--- trunk/src/backsubst.c
+++ trunk/src/backsubst.c
@@ -4,19 +4,24 @@
  * Zwraca 1 - błąd dzielenia przez 0 (element na diagonali = 0)
  * Zwraca 2 - błąd nieprawidłowych rozmiarów macierzy
  */
-int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
-				/**
-				 * Tutaj należy umieścić właściwą implemntację.
-				 */
+int backsubst(Matrix *x, Matrix *mat, Matrix *b) {
+    if (mat->r != mat->c || mat->r != b->r || b->c != 1 || x->r != b->r || x->c != 1) {
+        return 2; // Błąd nieprawidłowych rozmiarów macierzy
+    }
 
-				/* To ponizej jest przepisaniem b do x. Nalezy to poprawic! */
+    for (int i = mat->r - 1; i >= 0; i--) {
+        if (mat->data[i][i] == 0) {
+            return 1; // Dzielenie przez 0 (element na diagonali = 0)
+        }
 
-				int i;
-				for (i =0; i < x->r; i++) {
-								x->data[i][0] = b->data[i][0];
-				}
+        double sum = 0.0;
+        for (int j = i + 1; j < mat->c; j++) {
+            sum += mat->data[i][j] * x->data[j][0];
+        }
 
-				return 0;
+        x->data[i][0] = (b->data[i][0] - sum) / mat->data[i][i];
+    }
+
+    return 0; // Wsteczne podstawienie zakończone sukcesem
 }
-
 
